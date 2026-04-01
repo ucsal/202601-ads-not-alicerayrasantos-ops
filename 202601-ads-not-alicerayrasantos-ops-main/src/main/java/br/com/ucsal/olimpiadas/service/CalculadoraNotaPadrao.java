89f02f850fb1f3a0e604b85fd1dfcba504e6c0d4
unknown <alicerayra.santos@ucsal.edu.br>
2026-04-01 14:31:39 -0300

    refactor: aplica OCP utilizando CalculadoraNota no App

---
+++ 202601-ads-not-alicerayrasantos-ops-main/src/main/java/br/com/ucsal/olimpiadas/service/CalculadoraNotaPadrao.java
@@ -0,0 +1,24 @@
+package br.com.ucsal.olimpiadas.service;
+
+public class CalculadoraNotaPadrao {
+    
+}
+package br.com.ucsal.olimpiadas.service;
+
+import br.com.ucsal.olimpiadas.Tentativa;
+
+public class CalculadoraNotaPadrao implements CalculadoraNota {
+
+    @Override
+    public int calcular(Tentativa tentativa) {
+        int acertos = 0;
+
+        for (var r : tentativa.getRespostas()) {
+            if (r.isCorreta()) {
+                acertos++;
+            }
+        }
+
+        return acertos;
+    }
+}
